93ff0e3602a262bff907eafc45663cfe82c1a9af
Sebastia-Munar-Alcover <smunaralcover11@gmail.com>
2022-04-07 09:20:18 +0200

    displayTaulell

--- src/Taulell.java
+++ src/Taulell.java
@@ -7,11 +7,11 @@ public class Taulell {
         switch (dificultat) {
             case "dificil" -> {
                 caselles = new Casella[16][32];
-                mines =99;
+                mines = 99;
             }
             case "intermig" -> {
                 caselles = new Casella[16][16];
-                mines =40;
+                mines = 40;
             }
             case "facil" -> {
                 caselles = new Casella[8][8];
@@ -20,52 +20,56 @@ public class Taulell {
         }
         omplirCaselles();
         omplirMines();
+        assignarValorCaselles();
     }
 
     public Casella[][] getCaselles() {
         return caselles;
     }
 
-    public void omplirCaselles(){
+    private void omplirCaselles() {
         for (int i = 0; i < caselles.length; i++) {
             for (int j = 0; j < caselles[0].length; j++) {
-                caselles[i][j]= new Casella();
+                caselles[i][j] = new Casella();
             }
         }
     }
 
-    public void omplirMines(){
-        while(mines!=0) {
+    private void omplirMines() {
+        while (mines != 0) {
             for (int i = 0; i < caselles.length; i++) {
                 for (int j = 0; j < caselles[0].length; j++) {
-                    if( Math.round(Math.random()*10) == 5){
+                    if (Math.round(Math.random() * 10) == 5) {
                         caselles[i][j].setMina(true);
                         mines--;
                     }
-                    if(mines==0){
+                    if (mines == 0) {
                         break;
                     }
                 }
-                if(mines==0){
+                if (mines == 0) {
                     break;
                 }
             }
         }
     }
 
-    public void displayTaulell(){
-        for(int i=0; i<caselles[0].length; i++){
-            for(int j=0; j<caselles.length; j++){
-                if(j== caselles.length-1) {
+    public void displayTaulell() {
+        for (int i = 0; i < caselles[0].length; i++) {
+            for (int j = 0; j < caselles.length; j++) {
+                if (j == caselles.length - 1) {
                     System.out.print("+---+\n");
-                }
-                else{
+                } else {
                     System.out.print("+---");
                 }
             }
-            for(int j=0; j<caselles.length; j++){
+            for (int j = 0; j < caselles.length; j++) {
 
-                if(caselles[i][j].isDestapada()) {
+                caselles[0][0].setBandera(true);
+                caselles[1][0].setBandera(true);
+                caselles[0][1].setBandera(true);
+
+                if (caselles[i][j].isDestapada()) {
                     if (j == caselles.length - 1) {
                         System.out.print(" | " + caselles[i][j].getValor() + " |\n");
                     } else if (j == 0) {
@@ -73,17 +77,15 @@ public class Taulell {
                     } else {
                         System.out.print(" | " + caselles[i][j].getValor());
                     }
-                }
-                else if(caselles[i][j].isBandera()){
+                } else if (caselles[i][j].isBandera()) {
                     if (j == caselles.length - 1) {
-                        System.out.print(" | " + 'I' + " |\n");
+                        System.out.print(" |" + " B " + "|\n");
                     } else if (j == 0) {
-                        System.out.print("| " + 'I');
+                        System.out.print("|" + " B ");
                     } else {
-                        System.out.print(" | " + 'I');
+                        System.out.print("|" + " B ");
                     }
-                }
-                else{
+                } else {
                     if (j == caselles.length - 1) {
                         System.out.print("|" + "[ ]" + "|\n");
                     } else if (j == 0) {
@@ -94,12 +96,11 @@ public class Taulell {
                 }
             }
 
-            if(i== caselles[0].length-1){ //Darrera línea d'abaix
-                for(int j=0; j<caselles.length; j++){
-                    if(j== caselles.length-1) {
+            if (i == caselles[0].length - 1) { //Darrera línea d'abaix
+                for (int j = 0; j < caselles.length; j++) {
+                    if (j == caselles.length - 1) {
                         System.out.print("+---+\n");
-                    }
-                    else{
+                    } else {
                         System.out.print("+---");
                     }
                 }
@@ -107,5 +108,36 @@ public class Taulell {
         }
 
         System.out.println("\n\n\n");
+    }
+
+    private void assignarValorCaselles() { //ordre: esquerra, adalt, dreta, abaix i diagonals començant per adalt a l'esquerra
+        for (int i = 0; i < caselles.length; i++) {
+            for (int j = 0; j < caselles[0].length; j++) {
+                if (i != 0 && caselles[i - 1][j].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (j != 0 && caselles[i][j - 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (i != caselles.length - 1 && caselles[i + 1][j].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (j != caselles[0].length - 1 && caselles[i][j + 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (i != 0 && j != 0 && caselles[i - 1][j - 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (i != caselles.length - 1 && j != 0 && caselles[i + 1][j - 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (i != caselles.length - 1 && j != caselles[0].length - 1 && caselles[i + 1][j + 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+                if (i != 0 && j != caselles[0].length - 1  && caselles[i - 1][j + 1].isMina()) {
+                    caselles[i][j].setValor(caselles[i][j].getValor() + 1);
+                }
+            }
+        }
     }
 }
