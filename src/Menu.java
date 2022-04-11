4bf84631c8c065722e1e313023678d83a2786b4c
Sebastia-Munar-Alcover <smunaralcover11@gmail.com>
2022-04-11 13:17:16 +0200

    Comprovació commit anterior

--- src/Menu.java
+++ src/Menu.java
@@ -1,9 +1,11 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Menu {
 
     private String dificultat;
     Scanner sc = new Scanner(System.in);
+    Taulell taulell = new Taulell(getDificultat());
 
     public String getDificultat() {
         return dificultat;
@@ -16,7 +18,7 @@ public class Menu {
         System.out.println("dificil -> 16x32 99 mines");
         System.out.println();
 
-        while(dificultat== null) {
+        while (dificultat == null) {
             switch (sc.nextLine()) {
                 case "dificil":
                     dificultat = "dificil";
@@ -36,20 +38,82 @@ public class Menu {
         }
     }
 
+    public void demanarAccio() {
+        System.out.println("Trïa una acció:");
+        System.out.println("1 -> Posar bandera");
+        System.out.println("2 -> Destapar casella");
+
+        String accio = "";
+
+        int fila;
+        int columna;
+
+        while (accio != "1" && accio != "2") {
+            accio = sc.nextLine();
+
+            switch (sc.nextLine()) {
+                case "1":
+                    System.out.println("Fila:");
+                    fila = sc.nextInt();
+                    System.out.println("Columna:");
+                    columna = sc.nextInt();
+                    taulell.posarBandera(fila, columna);
+                    break;
+
+                case "2":
+                    System.out.println("Fila:");
+                    fila = sc.nextInt();
+                    System.out.println("Columna:");
+                    columna = sc.nextInt();
+                    taulell.destaparCasella(fila, columna);
+                    break;
+
+                default:
+                    System.out.println("Acció no vàlida");
+            }
+        }
+    }
+
+
     public void guanyar() {
         System.out.println("EHORABONA, HAS GUANYAT!!");
         System.exit(0);
     }
 
-    public void perdre(Taulell taulell) {
+    public void perdre() {
         System.out.println("LLÀSTIMA, HAS PERDUT!!");
         taulell.displayMines();
         System.exit(0);
     }
 
+    public void comprovarDerrota() {
+        Casella[][] caselles = taulell.getCaselles();
 
+        for (int i = 0; i < caselles.length; i++) {
+            for (int j = 0; j < caselles[0].length; j++) {
+                if (caselles[i][j].isMina() && caselles[i][j].isDestapada()) {
+                    perdre();
+                }
+            }
+        }
+    }
 
+    public void comprovarVictoria() {
+        Casella[][] caselles = taulell.getCaselles();
+        boolean isVictoria = true;
 
+        for (int i = 0; i < caselles.length; i++) {
+            for (int j = 0; j < caselles[0].length; j++) {
+                if (!caselles[i][j].isMina() && !caselles[i][j].isDestapada()) {
+                    isVictoria = false;
+                }
+            }
+        }
+
+        if (isVictoria) {
+            guanyar();
+        }
+    }
 
 
 }
